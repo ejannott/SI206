b98c555cd44d8cc2d1159764608853cdb2e41e49
Ethan Jannott <edog51909@Ethans-MacBook-Air.local>
2016-11-12 02:13:50 -0500

    finished bshw3

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -11,5 +11,22 @@
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
 import requests
-from bs4 import BeauifulSoup
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 
+# Prepare the site for use with BeautifulSoup
+base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+html = urllib.request.urlopen(base_url).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# prettify turns the text contained by 'soup' into a string formated like an html
+htmlText = soup.prettify()
+
+# apply replacements to the html text in htmlText
+htmlText = htmlText.replace('student', "AMAZING student")
+htmlText = htmlText.replace('https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg', 'ethan206pic.jpg')
+# # # not totally sure why the below replaces local images on the page
+htmlText = htmlText.replace('logo2.png', 'media/logo.png')
+
+file = open('projectpage.html', 'w')
+file.write(htmlText)
